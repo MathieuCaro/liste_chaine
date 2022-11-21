2081a1fab923cd15d79499251a710fb77710045d
MathieuC98 <mathieucaro123@outlook.com>
2022-11-21 09:42:55 +0100

    premier commit

---
+++ fonctions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "fonctions.h"
+
+int bonjour(int b)
+{
+    int a = 4;
+    return a;
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "fonctions.h"
+
+int main()
+{
+    bonjour();
+    return 0;
+}
